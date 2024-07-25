988ab19a487ea43dcf82347891dc2fac0af27181
DaVinci <d.gisanura@alustudent.com>
2024-07-25 03:31:02 -0700

    print a matrix list

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
-            print("{:d}".format(j), end=' ' if j !=[-1] else '')
+            for j in i:
+                print("{:d}".format(j), end=' ' if j !=[-1] else '')
         print()
