3fd24c2789fb99abb68d148e621e8a0b9938ff47
DaVinci <d.gisanura@alustudent.com>
2024-07-31 04:17:23 -0700

    1-hbtn_header.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-This module fetches the status from a given URL and displays the body response.
-"""
-
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'http://0.0.0.0:5050/status'
+url = 'https://alu-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+with urllib.request.urlopen(url) as response:
+    body = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
