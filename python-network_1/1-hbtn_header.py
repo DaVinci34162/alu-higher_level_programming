928bb6494b8aa2c01e925ff94bbf5c583b1a20ce
DaVinci <d.gisanura@alustudent.com>
2024-07-31 08:22:37 -0700

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
+'''value of the X-Request-Id variable found in the header'''
+
+
 import urllib.request
 import sys
 
-def fetch_request_id(url):
-    with urllib.request.urlopen(url) as response:
-        headers = response.getheaders()
-        for header in headers:
-            if header[0] == 'X-Request-Id':
-                print(header[1])
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    fetch_request_id(url)
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        header = res.info()
+        print(header['X-Request-Id'])
