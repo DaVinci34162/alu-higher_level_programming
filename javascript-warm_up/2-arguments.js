e10a6b2adaa376ed588367f52315131463d88376
EliseJulio <j.hakiziman1@alustudent.com>
2024-09-26 00:01:13 +0200

    New

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-if (argv.length < 3) {
-    console.log('No argument');
-} else if (argv.length > 3) {
-    console.log('Argument found');
+if (argv.length === 0) {
+  console.log('No argument');
+} else if (argv.length === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
