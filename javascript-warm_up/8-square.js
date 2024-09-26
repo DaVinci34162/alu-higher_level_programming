bd51d861ede186ddfa23c743029a137710db385e
EliseJulio <j.hakiziman1@alustudent.com>
2024-09-26 02:20:10 +0200

    New

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const num = process.argv.slice(2);
+const x = Math.floor(num);
+
+
+if (x){
+    for (let i = 0; i < x; i++) {
+        console.log('X'.repeat(x));
+    }
+}
+else if (x < 0){
+    console.log();
+}else (x === undefined || isNaN(x))
+{
+    console.log('Missing size');
+}
