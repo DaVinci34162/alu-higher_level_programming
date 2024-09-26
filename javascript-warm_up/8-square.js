bf1b9f4c4ac0516fa981610dc2c207a4da0165ad
EliseJulio <j.hakiziman1@alustudent.com>
2024-09-26 02:23:48 +0200

    New

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,14 +3,12 @@
 const num = process.argv.slice(2);
 const x = Math.floor(num);
 
-
-if (x){
-    for (let i = 0; i < x; i++) {
-        console.log('X'.repeat(x));
-    }
-}
-else if (x < 0){
-    console.log();
-}else {
-    console.log('Missing size');
+if (x) {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+} else if (x < 0) {
+  console.log();
+} else {
+  console.log('Missing size');
 }
